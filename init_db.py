b9f682909c9d60b8a9ca96ff8d2bbbfc87895d9c
megamot <butan85@gmail.com>
2025-10-12 11:02:43 +0300

    [Update] add list of companies

--- init_db.py
+++ init_db.py
@@ -13,11 +13,36 @@ company_id_collection = db.company_ids
 # ВАШ ПОВНИЙ СПИСОК ID КОМПАНІЙ
 # Додайте всі ID, які ви зібрали, до цього списку:
 FULL_COMPANY_ID_LIST: List[int] = [
-    6627493,  # SKELAR    
+    6627493, # SKELAR    
     782154,  # Genesis
-    # ДОДАЙТЕ ВСІ ВАШІ КОМПАНІЇ СЮДИ
-    5319794,  # FRACTAL (ex-Netpeak Group)
-    # ...
+    5319794, # FRACTAL (ex-Netpeak Group)
+    757252,  # Netpeak
+    825,     # Sigma Software
+    43767,   #N-iX
+    1184305, #ZONE3000
+    6129277, #Squad
+    14837661, #Headway
+    15539827, #inVerita
+    15906383, #1590638
+    806,     #Vodafone Україна
+    1074,    #EPAM Systems
+    1063,    #Philip Morris
+    794,     #GlobalLogic
+    874,     #Київстар
+    1006322, #Farmak
+    11789504, #OBRIO
+    7819611, #appflame
+    606229,  #Дарниця
+    1822103, #Імперіал Тобакко
+    927691, #ACINO
+    381928, #Carlsberg Ukraine
+    209907, #Ciklum
+    277662, #DataArt
+    567736, #KPMG
+    837,    #PwC
+    826651,   #Deloitte
+    1129,   #EY
+    825,    #Sigma Software
 ]
 
 def insert_new_company_batch(new_ids: List[int]):
